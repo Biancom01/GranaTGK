a8315d00ea83fe3da441c7561fd5d38d96fd1827
Krykspl <104348572+Krykspl@users.noreply.github.com>
2024-10-17 15:21:03 +0200

    predkosc v2
    xxxx

--- DeadlyWhalesGame/Assets/Scenes/EnemyFollow.cs
+++ DeadlyWhalesGame/Assets/Scenes/EnemyFollow.cs
@@ -6,10 +6,16 @@ public class EnemyFollow : MonoBehaviour
     public Transform player; // Referencja do gracza
     private NavMeshAgent agent;
 
+    [SerializeField]
+    private float speed = 3.5f; // Prêdkoœæ przeciwnika (mo¿na dostosowaæ w edytorze)
+
     void Start()
     {
         // Znajdujemy NavMeshAgent
         agent = GetComponent<NavMeshAgent>();
+
+        // Ustawiamy prêdkoœæ agenta
+        agent.speed = speed;
 
         // Jeœli nie przypisaliœmy gracza w edytorze, szukamy go automatycznie po tagu "Player"
         if (player == null)
